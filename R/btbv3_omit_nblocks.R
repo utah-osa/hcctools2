9695d401f86d1db1dfbe9dc2ef8ae58d4890d304
alexandernielson <alexnielson@utah.gov>
2020-09-15 11:31:57 -0600

    trying namespace fix

--- R/btbv3_omit_nblocks.R
+++ R/btbv3_omit_nblocks.R
@@ -18,17 +18,17 @@ btbv3_omit_nblocks <- function(df){
     ),
     .f=create_clean_sp_bp_names_no_anes)
   )] %>%
-    .[,`:=`(
+    data.table::.[,`:=`(
       doctor_str = cleaned_names %>% purrr::map_chr(~.[[1]]),
       facility_str = cleaned_names %>% purrr::map_chr(~.[[2]])
     )] %>%
-    .[,.(
+    data.table::.[,.(
       tp_med_med = median(tp_med),
       tp_cnt_cnt = sum(cnt)
     ),by=c("doctor_str","facility_str")] %>%
-    .[, data.table::`:=`(
+    data.table::.[, data.table::`:=`(
       num_doc = doctor_str %>% purrr::map_int(find_extra_doctors),
       hospital_detects = facility_str %>% purrr::map_int(filter_strange_hosp_combo)
     )] %>%
-    .[num_doc < 3 & hospital_detects < 2 ]
+    data.table::.[num_doc < 3 & hospital_detects < 2 ]
 }
